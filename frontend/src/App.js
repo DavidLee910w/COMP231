37ea73baab05b5435a5357eb568a86befba599ee
Wing Tung Lee <wlee171@my.centennialcollege.ca>
2025-03-13 14:31:12 -0400

    second commit
    revised base code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,28 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
 // frontend/src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -7,18 +32,18 @@ import RecipeForm from './components/RecipeForm';
 import AdminDashboard from './components/AdminDashboard';
 
 function App() {
-    return (
-        <Router>
-            <div>
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/search" element={<RecipeSearch />} />
-                    <Route path="/recipe/new" element={<RecipeForm />} />
-                    <Route path="/admin" element={<AdminDashboard />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/search" element={<RecipeSearch />} />
+          <Route path="/recipe/new" element={<RecipeForm />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
